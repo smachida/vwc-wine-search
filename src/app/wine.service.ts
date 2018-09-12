a11d9dbefb78ab63827e9b5dcabf2ff3d383c26d
Shuichi Machida <shuichi.machida@gmail.com>
2018-09-12 23:24:28 +0900

    created

---
+++ src/app/wine.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from "@angular/core";
+import {HttpParams, JsonpClientBackend, HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/map";
+
+export interface WineData {
+  error: string;
+  data: string;
+}
+
+@Injectable()
+export class WineService {
+  WEB_API_URL: string = "http://ec2-52-192-145-111.ap-northeast-1.compute.amazonaws.com:28080/api/v1/wine/wines";
+  //DEFAULT_SIZE: string = "30";
+
+  constructor(private http: HttpClient) {
+  }
+
+  getWineData(countryCode: string): Observable<WineData> {
+    let config = this.setParam(countryCode);
+    return this.reqData(config);
+  }
+
+  setParam(countryCode: string) : HttpParams {
+    // Url params
+    return (new HttpParams())
+      .set("countryCode", countryCode)
+      //.set("count", this.DEFAULT_SIZE)
+  }
+
+  reqData(config): Observable<WineData> {
+    let url = this.WEB_API_URL + "?" + config.toString();
+    return this.http.get(url)
+      .map(res => {
+          let wineData;
+          let dataObj = res;
+          wineData = {
+            error: null,
+            data: dataObj,
+          };
+
+          console.dir(wineData);
+          return wineData;
+        }
+      );
+  }
+}
